60b9719ba928b7ff5718fbb208cc59afffbfea98
Christian Otkjær <christianotkjaer@icloud.com>
2017-10-24 13:12:11 +0200

    Initial Commit

---
+++ Primes/Primes.h
@@ -0,0 +1,19 @@
+//
+//  Primes.h
+//  Primes
+//
+//  Created by Christian Otkjær on 24/10/2017.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Primes.
+FOUNDATION_EXPORT double PrimesVersionNumber;
+
+//! Project version string for Primes.
+FOUNDATION_EXPORT const unsigned char PrimesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Primes/PublicHeader.h>
+
+
